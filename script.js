bc49eb3ae57e76aa094c1af866f97e2350deade7
Patrick Sterz <82339311+oCVamous@users.noreply.github.com>
2022-08-22 12:40:17 +0200

    Layout with MDB

--- script.js
+++ script.js
@@ -1,92 +1,11 @@
-const url = 'https://pokeapi.co/api/v2/pokemon/';
-
 function init() {
-    loadAllPokemons();
+    loadAllPokemon();
 }
 
-async function loadAllPokemons() {
+async function loadAllPokemon() {
     let url = 'https://pokeapi.co/api/v2/pokemon/';
     let response = await fetch(url);
     let responseAsJSON = await response.json();
 
     console.log('Loaded pokemon', responseAsJSON);
 }
-function showMyBtn() {
-    document.getElementById('show-btn').innerHTML += 
-    `
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-    `;
-}
-
-function showMoreInformation() {
-    document.getElementById('moreInformationBox').innerHTML += `
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          ...
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-      </div>
-    </div>
-  </div>
-    `;
-}
-/***
- * function renderStartModal() {
-
-    var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
-
-    let content = document.getElementById('content');
-    content.innerHTML = '';
-    content.innerHTML += `
-    <div class="modal" tabindex="-1">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Modal title</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <p>Modal body text goes here.</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-      </div>
-    </div>
-  </div>
-    `;
-}
- * 
- */
